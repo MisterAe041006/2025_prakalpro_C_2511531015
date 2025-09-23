c5a363c810f17af668c6df65446700261d4e3dfa
user <user@LAPTOP-FSGICB3R>
2025-09-23 12:01:16 +0700

    pekan3 praktikum kelas c

---
+++ prakalpro_2025_c_2511531015/src/pekan3/OperatorRelasional.java
@@ -0,0 +1,34 @@
+package pekan3;
+
+import java.util.Scanner;
+
+public class OperatorRelasional {
+
+	public static void main(String[] args) {
+		int A1;
+		int A2;
+		boolean hasil;
+		Scanner keyboard = new Scanner (System.in);
+		System.out.print("Input angka-1: ");
+		A1 = keyboard.nextInt();
+		System.out.print("input ankga-2: ");
+		A2 = keyboard.nextInt();
+	    keyboard.close();
+	    hasil = A1 > A2;// apakah A1 lebih besar A1?
+	    System.out.println("A1 > A2 = " +hasil);
+	    hasil = A1 < A2;// apakah A1 lebih kecil A2? 
+	    System.out.println("A1 < A2 = " +hasil);
+	    hasil = A1 >=  A2;//apakah A1 besar sama dengan A2?
+	    System.out.println("A1 >= A2 = " +hasil);
+	    hasil = A1 <= A2;// apakah A1 kecil sama dengan A2?
+	    System.out.println("A1 <= A2 = " +hasil);
+	    hasil = A1 == A2;// apakah hasil A1 saa dengan A2/
+	    System.out.println("A1 == A2 = " +hasil);
+	    hasil = A1 != A2;// apakah hasil A1 tidak sama dengan A2?
+	    System.out.println("A1 != A2 = " +hasil);
+	    
+	    		
+
+	}
+
+}
