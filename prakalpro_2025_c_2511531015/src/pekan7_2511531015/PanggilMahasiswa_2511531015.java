029263f782e545dbc35f38d590f2948c246c09c0
MisterAe041006 <fahrisyahputra041006@gmail.com>
2025-11-11 11:52:13 +0700

    Add files via upload
    prakalpro pekan 7 2511531015

---
+++ prakalpro_2025_c_2511531015/src/pekan7_2511531015/PanggilMahasiswa_2511531015.java
@@ -0,0 +1,15 @@
+package pekan7_2511531015;
+
+public class PanggilMahasiswa_2511531015 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511531015 a= new Mahasiswa_2511531015();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+
+	}
+
+}
---
+++ prakalpro_2025_c_2511531015/src/pekan7_2511531015/String1_2511531015.java
@@ -0,0 +1,15 @@
+package pekan7_2511531015;
+
+public class String1_2511531015 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("panjang salam adalah :" + salam.length());
+		System.out.println(salam.toUpperCase()); // outputs "ASSALAMUALAIKUM"
+		System.out.println(salam.toLowerCase()); // output "assalamualaikum"
+		System.out.println(salam.indexOf("salam")); // outputs 2
+		
+
+	}
+
+}
